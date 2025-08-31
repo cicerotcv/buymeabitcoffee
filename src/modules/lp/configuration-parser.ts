1db5af1c028489e69ec8b22967da7a15f5fb757f
Cicero Tiago <49568618+cicerotcv@users.noreply.github.com>
2025-08-31 19:29:26 -0300

    fix: btcAddress validation in parser

--- src/modules/lp/configuration-parser.ts
+++ src/modules/lp/configuration-parser.ts
@@ -3,11 +3,7 @@ import { z } from 'zod';
 
 export const parser = z.object({
   identifier: z.string().min(2).max(100),
-  btcAddress: z
-    .string()
-    .min(42)
-    .max(42)
-    .refine((value) => validate(value)),
+  btcAddress: z.string().refine((value) => validate(value)),
   label: z.string().min(2).max(32).optional().or(z.literal('')),
   content: z.string().min(2).max(32),
 });
