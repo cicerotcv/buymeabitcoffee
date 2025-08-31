d3023a14b4250b95dd78fd18737cfa706eba1513
cicerotcv <elm.tiago@gmail.com>
2025-08-31 15:07:54 -0300

    style: format code for consistency in opengraph-image, robots, and sitemap files

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -24,11 +24,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly',
       priority: 0.5,
     },
-    {
-      url: `${Env.VercelUrl}/btc/bc1qw4q8nn7pknen33han7znsv6zhrrfta53sr86fw?identifier=Buy+Me+a+BitCoffee`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.2,
-    },
   ];
 }
